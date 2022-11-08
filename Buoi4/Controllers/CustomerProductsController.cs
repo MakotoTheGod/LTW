8d997017eeba5b0db27b9ab5db940a149d473366
MakotoTheGod <minhlong882003@gmail.com>
2022-11-08 18:43:52 +0700

    first commit

---
+++ Buoi4/Controllers/CustomerProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Buoi4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace Buoi4.Controllers
+{
+    public class CustomerProductsController : Controller
+    {
+        // GET: CustomerProducts
+        private DBSportStoreEntities1 db = new DBSportStoreEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new
+               HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
